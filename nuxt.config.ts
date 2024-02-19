575ddf8024473816e32d93ec2ea844ded6f53772
dachsification <tim.arendt@arcor.de>
2024-02-19 18:06:03 +0100

    removed nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@nuxt/ui-pro'],
   typescript: {
     typeCheck: true,
   },
   devtools: { enabled: true },
-  modules: [['@nuxtjs/eslint-module', { fix: true }], '@vueuse/nuxt', '@nuxtjs/i18n', '@nuxt/ui', '@pinia/nuxt'],
+  modules: [['@nuxtjs/eslint-module', { fix: true }], '@vueuse/nuxt', '@nuxtjs/i18n', '@pinia/nuxt'],
 });
